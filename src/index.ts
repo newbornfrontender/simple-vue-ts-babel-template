479f1c1df0f7e8da70ee0bafe74acf2a8357a3a2
Infant Frontender <newbornfrontender@gmail.com>
2018-08-02 10:08:45 +0300

    Добавил тестовые файлы для проверки корректности работы webpack

---
+++ src/index.ts
@@ -0,0 +1,7 @@
+'use strict';
+
+import './index.css';
+
+console.log(`NODE_ENV=${process.env.NODE_ENV}`);
+console.log(`HASH_DIGEST=${process.env.HASH_DIGEST}`);
+console.log(`HASH_DIGEST_LENGTH=${process.env.HASH_DIGEST_LENGTH}`);
